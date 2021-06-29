e84973006d478c44dfeeee9d0dcf5370c8aaf9c0
luchao <1967768885@qq.com>
2021-06-29 23:31:29 +0800

    子域名访问计数.

---
+++ category/三、哈希表、集合、LRU/SubdomainVisitCount.java
@@ -0,0 +1,44 @@
+package leetcode.editor.cn;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * [811]子域名访问计数
+ * @author luchao
+ */
+public class SubdomainVisitCount {
+    public static void main(String[] args) {
+        Solution solution = new SubdomainVisitCount().new Solution();
+    }
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    /**
+     * 主体：多级域名 + 哈希表计数
+     * 细节：正则
+     */
+    public List<String> subdomainVisits(String[] cpdomains) {
+        // key - 域名，value - 计数
+        HashMap<String, Integer> map = new HashMap<>();
+        for (String domain : cpdomains) {
+            String[] arr = domain.split("\\s+");
+            Integer count = Integer.valueOf(arr[0]);
+            String[] frags = arr[1].split("\\.");
+            // 多级域名计数
+            String level = "";
+            for (int i = frags.length - 1; i >= 0; i--) {
+                level = frags[i] + (i == frags.length - 1 ? "" : ".") + level;
+                map.put(level, map.getOrDefault(level, 0) + count);
+            }
+        }
+        // 构造ans
+        List<String> ans = new ArrayList<>();
+        map.forEach((key, value) -> ans.add(value + " " + key));
+        return ans;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
