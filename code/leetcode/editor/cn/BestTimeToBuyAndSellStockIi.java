e1b40dc5260e9f4ba3437836a15952f605eba31f
luchao <1967768885@qq.com>
2021-08-31 15:24:45 +0800

    贪心.

--- code/leetcode/editor/cn/BestTimeToBuyAndSellStockIi.java
+++ code/leetcode/editor/cn/BestTimeToBuyAndSellStockIi.java
@@ -12,31 +12,15 @@ public class BestTimeToBuyAndSellStockIi {
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     /**
-     * 特征：买卖股票系列问题，DP
-     *
-     * 思路：通解DP
-     * 1.定义状态：dp[i][0]表示第i天结束时没有股票的最大利润，dp[i][1]表示第i天结束时持有股票的最大利润。
-     * 2.状态转移方程：
-     *  dp[i][0] = max(dp[i - 1][0], dp[i - 1][1] + prices[i])
-     *  dp[i][1] = max(dp[i - 1][1], dp[i - 1][0] - prices[i])
-     * 3.边界：dp[0][0] = 0, dp[0][1] = - prices[0]
-     * 4.目标：最后一天不持有 - dp[length - 1][0]
-     *
-     * 优化：贪心
+     * 思路：贪心。今天比昨天的价格高就交易。
+     * 注意：贪心只能计算最大利润，而非实际的交易过程
      */
     public int maxProfit(int[] prices) {
-        int length = prices.length;
-        int[][] dp = new int[length][2];
-        // 边界
-        dp[0][0] = 0;
-        dp[0][1] = - prices[0];
-        // DP
-        for (int i = 1; i < length; i++) {
-            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
-            dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] - prices[i]);
+        int ans = 0;
+        for (int i = 1; i < prices.length; i++) {
+            ans += Math.max(prices[i] - prices[i - 1], 0);
         }
-        // 目标，最后一天不持有
-        return dp[length - 1][0];
+        return ans;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
